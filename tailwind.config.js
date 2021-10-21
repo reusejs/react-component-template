003ee359344be9640934f9248409a1209fa6e562
Rajiv Seelam <rajiv@betalectic.com>
2021-10-21 23:06:02 +0530

    Update tailwind config to include stories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.js", "./stories/**/*.*", "./.storybook/*.js"],
+  purge: ["./src/**/*.js", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
